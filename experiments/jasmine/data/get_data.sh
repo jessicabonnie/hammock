013b5383c6691eef8c5087878cf40db147d1132f
Jessica Bonnie <1863770+jessicabonnie@users.noreply.github.com>
2025-03-24 17:13:11 -0400

    initiate jasmine related data experiment

---
+++ experiments/jasmine/data/get_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the data from the server
+
+wget http://data.schatz-lab.org/jasmine/HG002Trio/HG002Trio_HiFi.merged.vcf.gz
+wget http://data.schatz-lab.org/jasmine/HG002Trio/README.txt
+wget http://data.schatz-lab.org/jasmine/HG002Trio/DeNovoDetection/denovo.merged.vcf.gz
+
+# Unzip the files
+
+
+
+# Run the script
+
